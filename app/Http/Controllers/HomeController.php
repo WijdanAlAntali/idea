2ced782a948fb2e2cb3e50b8e887c109dd00482d
WijdanObaid <wijdan.obaid@gmail.com>
2020-09-11 00:06:15 +0400

    finalproject

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Store;
+
+class HomeController extends Controller
+{
+    //
+    public function index(){
+        $stores = Store::all();
+        return view('welcome',compact('stores'));
+    }
+}
---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,3 @@
+<footer class=" pt-1 mt-4 border-top">
+      <p class="p-2 text-secondary text-uppercase">{{__('messages.madein')}} <a class="text-light" target="_blank" href="https://www.linkedin.com/in/wijdan-alantali/">{{__('messages.wijdan')}}</a> @2020</a>.</p>
+</footer>
